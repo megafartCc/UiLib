288dcf47ff2f274e637e7a2a47f338d1c2ca02bd
megafartCc <megafartCc@users.noreply.github.com>
2026-03-10 19:42:13 -0500

    Repin testing to smaller mobile window

--- testing.lua
+++ testing.lua
@@ -4,7 +4,7 @@
 ]]
 
 local function loadRemoteUiLib()
-    local uiLibCommit = "fe42c78"
+    local uiLibCommit = "b4a1417"
     local baseUrl = "https://raw.githubusercontent.com/megafartCc/UiLib/" .. uiLibCommit .. "/UILibModules"
     local moduleCache = {}
     local cacheBust = "cb=" .. tostring(os.clock()):gsub("%.", "")
